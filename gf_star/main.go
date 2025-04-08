c614ca2009dd2802964744b7735185c1cf2c0be1
moyuxing <moyuxing@kp.com>
2025-04-08 14:02:53 +0800

    feat(星辰英语本):[2-1]数据库连接

--- gf_star/main.go
+++ gf_star/main.go
@@ -1,13 +1,31 @@
 package main
 
 import (
+	"errors"
 	_ "gf_star/internal/packed"
-
+	_ "github.com/gogf/gf/contrib/drivers/mysql/v2"
+	"github.com/gogf/gf/v2/frame/g"
 	"github.com/gogf/gf/v2/os/gctx"
 
 	"gf_star/internal/cmd"
 )
 
 func main() {
+	var err error
+
+	// 检测数据库是否正常连接
+	err = connDb()
+	if err != nil {
+		panic(err)
+	}
+
 	cmd.Main.Run(gctx.GetInitCtx())
+}
+
+func connDb() error {
+	err := g.DB().PingMaster()
+	if err != nil {
+		return errors.New("数据库连接失败")
+	}
+	return nil
 }
