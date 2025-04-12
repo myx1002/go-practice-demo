db8574e844b5d5bbf7a2802dd10f4745a16a231f
莫宇行 <2422721397@qq.com>
2025-04-12 17:06:25 +0800

    feat(星辰英语本-微服务):用户服务注册到etcd

--- proxima/app/user/main.go
+++ proxima/app/user/main.go
@@ -1,14 +1,26 @@
 package main
 
 import (
-	_ "proxima/app/user/internal/packed"
-
-	"github.com/gogf/gf/v2/os/gctx"
-
 	_ "github.com/gogf/gf/contrib/drivers/mysql/v2"
+	"github.com/gogf/gf/contrib/registry/etcd/v2"
+	"github.com/gogf/gf/contrib/rpc/grpcx/v2"
+	"github.com/gogf/gf/v2/frame/g"
+	"github.com/gogf/gf/v2/os/gctx"
 	"proxima/app/user/internal/cmd"
+	_ "proxima/app/user/internal/packed"
 )
 
 func main() {
+	// 读取etcd配置
+	var ctx = gctx.New()
+	conf, err := g.Cfg("etcd").Get(ctx, "etcd.address")
+	if err != nil {
+		panic(err)
+	}
+
+	// 注册etcd
+	var address = conf.String()
+	grpcx.Resolver.Register(etcd.New(address))
+
 	cmd.Main.Run(gctx.GetInitCtx())
 }
